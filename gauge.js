d8212c70e3f46807e786d4e14c74190e6a4507a5
a <creightons26@gmail.com>
2017-01-31 23:02:05 -0500

    Added a reading value and animated the rotation of the needle

--- gauge.js
+++ gauge.js
@@ -106,16 +106,40 @@ var x = 150,
 	arcSections = 1,
 	tickLength = 60,
 	tickCount = 10;
-	
-var ticks = getTicks(x, y, radius, tickLength, tickCount);
-var arc = getArc(x, y, radius, arcGap, arcWidth, arcSections);
+function drawGauge(x, y, radius, arcWidth, arcGap, arcSections, tickLength, tickCount) {
+	var ticks = getTicks(x, y, radius, tickLength, tickCount);
+	var arc = getArc(x, y, radius, arcGap, arcWidth, arcSections);
+	var triangle = "<path id='needle' d='M145 95 L150 25 L155 95 Z' fill='green' stroke='green' stroke-width='1' />";
+	var reading = "<text id='reading' x='" + x + "' y='" + y + "' text-anchor='middle' font-size='35' />";
+	var palette = document.querySelector('#palette');
 
-var palette = document.querySelector('#palette');
-palette.innerHTML = '';
-palette.innerHTML += ticks;
-palette.innerHTML += arc;
+	palette.innerHTML = '';
+	palette.innerHTML += ticks;
+	palette.innerHTML += arc;
+	palette.innerHTML += triangle;
+	palette.innerHTML += reading;
+}
 
+drawGauge(x, y, radius, arcWidth, arcGap, arcSections, tickLength, tickCount);
 
-var triangle = "<path d='M145 95 L150 25 L155 95 Z' fill='green' stroke='green' stroke-width='1' />";
+var needle = document.getElementById('needle'),
+	reading = document.getElementById('reading');
 
-palette.innerHTML += triangle;
+needle.style.transform = 'rotate(-90deg)';
+needle.style.transformOrigin =`${x}px ${y}px`;
+needle.style.transition = 'transform 0.7s';
+reading.innerHTML = '0';
+
+function changeAngle(needle, reading) {
+	// Subtract .5 from the random number to make the angle
+	// vary between -90deg and 90deg.
+	var angle = 180 * (Math.random() - 0.5);
+	// Convert angle to a value from 0 to 100.
+	// Also, need to make sure angle ranges from 0 to 180.
+	var value = (angle + 90) * 100 / 180;
+	needle.style.transform = `rotate(${angle}deg)`;
+	reading.innerHTML = value.toFixed();
+}
+
+setInterval(function() { changeAngle(needle, reading); }, 2000);
+
